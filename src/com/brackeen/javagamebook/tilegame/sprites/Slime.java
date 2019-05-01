469fbb93f0963dd45f1a612cb0f6fd708989b13e
Brandon Carvalho <32946299+bcarvalho2500@users.noreply.github.com>
2019-05-01 13:49:52 -0400

    Adds new images and classes

---
+++ src/com/brackeen/javagamebook/tilegame/sprites/Slime.java
@@ -0,0 +1,32 @@
+package com.brackeen.javagamebook.tilegame.sprites;
+
+import com.brackeen.javagamebook.codereflection.CodeReflection;
+import com.brackeen.javagamebook.graphics.Animation;
+
+public class Slime extends Grub {
+	
+	public Slime(Animation left, Animation right, Animation deadLeft, Animation deadRight) {
+		super(left,right,deadLeft,deadRight);
+		
+		if(CodeReflection.isTracing() && SpritesPackageTracingEnabled.getSpritesPackageTracingEnabledInstance().isEnabled()) {
+        	if(CodeReflection.getAbstactionLevel()>=1)
+        	{//check to make sure it's this level of abstraction
+        		e.fillInStackTrace();		
+        		CodeReflection.registerMethod(e.getStackTrace()[0].getClassName(),
+        								e.getStackTrace()[0].getMethodName());
+        	}
+    	}
+	}
+
+	public float getMaxSpeed() {
+    	if(CodeReflection.isTracing() && SpritesPackageTracingEnabled.getSpritesPackageTracingEnabledInstance().isEnabled()) {
+        	if(CodeReflection.getAbstactionLevel()>=2)
+        	{//check to make sure it's this level of abstraction
+        		e.fillInStackTrace();		
+        		CodeReflection.registerMethod(e.getStackTrace()[0].getClassName(),
+        								e.getStackTrace()[0].getMethodName());
+        	}
+    	}
+        return 0.03f * enemySpeedMultiplier;
+    }
+}
